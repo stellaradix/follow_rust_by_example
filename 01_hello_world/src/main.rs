e2486d2feb64fbd86dd9f46ccc63b6a83b7d45d2
stellaradix <111672276+stellaradix@users.noreply.github.com>
2023-01-30 10:24:18 +0900

    01 Hello World

---
+++ 01_hello_world/src/main.rs
@@ -0,0 +1,17 @@
+// traditional Hello World Program
+
+// main function
+fn main() {
+	// Statements here are executed when the compiled binary is called
+
+	// println! is a macro that prints text to the console
+	println!("Hello, world!");
+
+	println!("I`m a Rustacean!")
+}
+
+// A binary can be generated using the Rust compiler: rustc
+// $ rustc sourceCodeFileName
+
+// binary can be executed
+// $ ./binaryFileName
